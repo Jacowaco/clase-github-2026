cf4d77a3ad98c99d44434f023b7c5ac0c36b824f
Jacowaco <joaquinibarlucia@gmail.com>
2026-05-11 20:15:16 -0300

    proyecto de caminante pegado encima

---
+++ caminante.js
@@ -0,0 +1,40 @@
+class Caminante{
+
+
+    constructor(){
+        this.x = random( width );
+        this.y = random( height );
+        this.t = 5;
+        this.vel = 4;
+        this.dir = random( TWO_PI );
+        push();
+        colorMode( HSB , 360 , 100 , 100 );
+        this.elColor = color( random(180,260) , 100 , 100 );
+        pop();
+    }
+
+    dibujar(){
+        push();
+        fill( this.elColor );
+        noStroke();
+        ellipse( this.x , this.y , this.t , this.t );
+        pop();
+    }
+
+    mover(){
+
+        this.dir += radians( random(-10,10) );
+
+        let dx = this.vel * cos( this.dir );
+        let dy = this.vel * sin( this.dir );
+
+        this.x += dx;
+        this.y += dy;
+        //         condicion.     ? valor-si.    : valor-no
+        this.x = ( this.x > width ? this.x-width : this.x );
+        this.x = ( this.x < 0 ? this.x+width : this.x );
+        this.y = ( this.y > height ? this.y-height : this.y );
+        this.y = ( this.y < 0 ? this.y+height : this.y );
+    }
+
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+
+let c = [];
+let cantidad = 1000;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+
+  for( let i=0 ; i<cantidad ; i++ ){
+    c[i] = new Caminante();
+  }
+
+  
+  background(0);
 }
 
 function draw() {
-  background(0,255,0);
+  push();
+  fill(0,5);
+  noStroke();
+  rect(0,0,width,height);
+  pop();
+  for( let i=0 ; i<cantidad ; i++ ){
+    c[i].mover();
+    c[i].dibujar();
+  }
 }
